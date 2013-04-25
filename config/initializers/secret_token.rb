8e17a6535eefc29f5cbfeaaca73817590a243fd8
siddtewari <sidtewari@gmail.com>
2013-04-25 17:19:01 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Y::Application.config.secret_token = '1d282d1388b396d80aea50ce7d197cbddeed3e64e453b2791fc6613fe9cb7624b0208c3b311eda13b92364b2a8ffe412349ca59a12d81b9f21dff2cfe8d0ed98'
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Y::Application.routes.draw do
+  # root :to => 'decks#index'
+  get "/decks" => 'decks#index'
+  get "/decks/new" => 'decks#new'
+  get "/decks/:id" => 'decks#show', as: :deck
+  post "/decks" => "decks#create"
+  get "/decks/:id/edit" => "decks#edit"
+  put "/decks/:id" => "decks#update"  
+  delete "/decks/:id" => "decks#destroy"
+end
